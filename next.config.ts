576aa9a1a97abb869add1a7562aff25027ca9034
Tarun <tarun7827@gmail.com>
2026-03-11 20:06:13 +0530

    Updated next.config to add missing remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -3,14 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
+      { protocol: "https", hostname: "images.unsplash.com" },
+      { protocol: "https", hostname: "placehold.co" },
+      { protocol: "https", hostname: "api.escuelajs.co" },
+      { protocol: "https", hostname: "png.pngtree.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+      { protocol: "https", hostname: "plus.unsplash.com" },
+      { protocol: "https", hostname: "picsum.photos" },
+      { protocol: "https", hostname: "img.freepik.com" },
+      { protocol: "https", hostname: "i.imgur.com" },
+      { protocol: "https", hostname: "www.freepik.com" },
+      { protocol: "https", hostname: "www.shutterstock.com" },
+      { protocol: "https", hostname: "placehol1d.co" },
+      { protocol: "https", hostname: "placeimg.com" },
+      { protocol: "https", hostname: "pravatar.cc" },
     ],
   },
 };
